f819bea61ffc53050b8320e929fcc936110a8ae2
dunmi9791 <dominic.anyanna@gmail.com>
2024-08-08 15:07:59 +0100

    config settings

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail', 'om_account_accountant', 'account'],
+    'depends': ['base', 'mail', 'om_account_accountant', 'account', 'sale'],
 
     # always loaded
     'data': [
